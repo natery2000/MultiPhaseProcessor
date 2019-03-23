022dd9a17560f033593e8d9dbabcc64d11afcd6c
Nathan Werry <natery2000@hotmail.com>
2019-03-23 14:13:47 -0500

    Removing HeadProcessee and TailProcessee

--- Natery.MultiPhaseProcessor/Natery.MultiPhaseProcessor.Test/Test_Processor.cs
+++ Natery.MultiPhaseProcessor/Natery.MultiPhaseProcessor.Test/Test_Processor.cs
@@ -1,6 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
@@ -29,16 +28,16 @@ namespace Natery.MultiPhaseProcessor.Test
                 return await Task.FromResult((int.Parse(i) + 1).ToString());
             };
 
-            Func<string, Task> third = (i) =>
+            Func<string, Task<string>> third = (i) =>
             {
                 list.Add(i);
-                return Task.CompletedTask;
+                return Task.FromResult(string.Empty);
             };
 
             processor
-                .WithHeadProcessee<string>(first, 1)
-                .WithProcessee<string, string>(second, 1)
-                .WithTailProcessee<string>(third, 1);
+                .WithProcessee(first, 1)
+                .WithProcessee(second, 1)
+                .WithProcessee(third, 1);
 
             processor.AddWorkItems(new[] { "10", "20", "30" });
 
@@ -66,31 +65,22 @@ namespace Natery.MultiPhaseProcessor.Test
                 return await Task.FromResult((int.Parse(i) + 1).ToString());
             };
 
-            Func<string, Task> third = (i) =>
+            Func<string, Task<string>> third = (i) =>
             {
                 list.Add(i);
-                return Task.CompletedTask;
+                return Task.FromResult(string.Empty);
             };
 
             processor
-                .WithHeadProcessee<string>(first)
+                .WithProcessee<string, string>(first)
                 .WithProcessee<string, string>(second)
-                .WithTailProcessee<string>(third);
+                .WithProcessee<string, string>(third);
 
             processor.AddWorkItems(new[] { "10", "20", "30" });
 
             await processor.BeginAsync();
 
             CollectionAssert.AreEquivalent(new string[] { "10", "11", "12", "20", "21", "22", "30", "31", "32" }, list);
-        }
-
-        [TestMethod]
-        [ExpectedException(typeof(Exception))]
-        public void Proccessor_AddNullHead_Exception()
-        {
-            var p = new Processor<int>();
-            p.WithHeadProcessee<int>((int i) => Task.FromResult(1));
-            p.WithHeadProcessee<int>((int i) => Task.FromResult(1));
         }
 
         //#14: Value types fail because of 'default' keyword for queue usage
@@ -109,8 +99,8 @@ namespace Natery.MultiPhaseProcessor.Test
             async Task TestFunction()
             {
                 var p = new Processor<int>();
-                p.WithHeadProcessee<int>((i) => Task.FromResult(1));
-                p.WithTailProcessee<int>((i) => Task.FromResult(1));
+                p.WithProcessee<int, int>((i) => Task.FromResult(1));
+                p.WithProcessee<int, int>((i) => Task.FromResult(1));
                 p.AddWorkItem(1);
 
                 await p.BeginAsync();
